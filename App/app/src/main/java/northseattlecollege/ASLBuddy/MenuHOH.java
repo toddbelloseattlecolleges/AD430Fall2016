72fc78759360382b054832ce4b598b02e51c76ba
Kellan Nealy <gellinn@uw.edu>
2016-10-17 17:26:44 -0700

    Added Buttons w/ Event Handlers:
    Added the 3 action buttons from App design to the HOH menu page.
    Now, you can log in with bar@example.com : world, and click these
    super legit buttons.  The functions that handle the button taps run when the event is triggered, but need to be wired up to the programming logic and support for Skype, and the database/client/Server API.

--- App/app/src/main/java/northseattlecollege/ASLBuddy/MenuHOH.java
+++ App/app/src/main/java/northseattlecollege/ASLBuddy/MenuHOH.java
@@ -2,6 +2,8 @@ package northseattlecollege.ASLBuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Author: Kellan Nealy
@@ -14,5 +16,67 @@ public class MenuHOH extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_hoh);
+
+        // Set main action button onClick attributes
+        Button hearingToolButton = (Button) findViewById(R.id.hearing_tool_button);
+        hearingToolButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startHearingTool();
+            }
+        });
+        Button videoRequestButton = (Button) findViewById(R.id.video_interpreter_button);
+        hearingToolButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestVideo();
+            }
+        });
+        Button physicalRequestButton = (Button) findViewById(R.id.physical_interpreter_button);
+        hearingToolButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestPhysical();
+            }
+        });
+        Button logoutButton = (Button) findViewById(R.id.logout_button);
+        hearingToolButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+    }
+
+    /**
+     * Starts new activity HearingTool.java
+     * Session information should persist while users are in the Hearing Tool
+     */
+    private void startHearingTool() {
+
+    }
+
+    /**
+     * Starts new video interpreter request through Skype
+     * Application may be dormant after the user starts the request
+     */
+    private void requestVideo() {
+
+    }
+
+    /**
+     * Starts new physical interpreter request through Skype
+     * Application may be dormant after the user starts the request
+     */
+    private void requestPhysical() {
+
+    }
+
+    /**
+     * Terminates this user's session and returns to the login/signup page
+     * Starts activity LoginActivity.java
+     */
+    private void logout() {
+
     }
 }
