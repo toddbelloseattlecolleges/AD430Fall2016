9c6bc1f9c27ff7ffdf8356ce6e2e71efe9db5f87
Jesse Bernoudy <jesse.bernoudy@gmail.com>
2016-10-31 19:00:44 -0700

    Added logic to determine if Request is Video or Physical and show radius appropriately

--- App/app/src/main/java/northseattlecollege/ASLBuddy/CreateRequest.java
+++ App/app/src/main/java/northseattlecollege/ASLBuddy/CreateRequest.java
@@ -1,6 +1,8 @@
 package northseattlecollege.ASLBuddy;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,7 +18,14 @@ import android.widget.Button;
  * Starts new video interpreter request through Skype.
  */
 
-public class CreateRequest extends AppCompatActivity {
+public class CreateRequest extends AppCompatActivity
+        implements SetRadiusFragment.OnSetRadiusFragmentInteractionListener {
+
+    public final static String REQUEST_TYPE = "northseattlecollege.ASLBuddy.REQUEST_TYPE";
+    public final static String REQUEST_TYPE_VIDEO = "northseattlecollege.ASLBuddy.REQUEST_TYPE_VIDEO";
+    public final static String REQUEST_TYPE_PHYSICAL = "northseattlecollege.ASLBuddy.REQUEST_TYPE_PHYSICAL";
+
+    private String requestType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +42,22 @@ public class CreateRequest extends AppCompatActivity {
                 CreateRequest.this.startActivity(navigationIntent);
             }
         });
+
+        // ToDo: Determine if this is a Video or Physical request
+        Intent intent = getIntent();
+        requestType = intent.getStringExtra(REQUEST_TYPE);
+
+        if(requestType.compareTo(REQUEST_TYPE_PHYSICAL) == 0) {
+            // ToDo: Get default/saved radius from Settings
+            SetRadiusFragment newFragment = SetRadiusFragment.newInstance(.25f);
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.setRadiusFragmentContainer, newFragment);
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(float radius) {
+
     }
 }
--- App/app/src/main/java/northseattlecollege/ASLBuddy/SetRadiusFragment.java
+++ App/app/src/main/java/northseattlecollege/ASLBuddy/SetRadiusFragment.java
@@ -1,7 +1,6 @@
 package northseattlecollege.ASLBuddy;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,25 +13,20 @@ import android.widget.TextView;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SetRadiusFragment.OnFragmentInteractionListener} interface
+ * {@link OnSetRadiusFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link SetRadiusFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class SetRadiusFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public static final String ARG_RADIUS = "radius";
 
     private SeekBar radiusValue;
     private TextView radiusDisplay;
+    private float radius;
 
-    private OnFragmentInteractionListener mListener;
+    private OnSetRadiusFragmentInteractionListener mListener;
 
     public SetRadiusFragment() {
         // Required empty public constructor
@@ -42,16 +36,13 @@ public class SetRadiusFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param radius Parameter 1.
      * @return A new instance of fragment SetRadiusFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static SetRadiusFragment newInstance(String param1, String param2) {
+    public static SetRadiusFragment newInstance(float radius) {
         SetRadiusFragment fragment = new SetRadiusFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putFloat(ARG_RADIUS, radius);
         fragment.setArguments(args);
         return fragment;
     }
@@ -60,10 +51,8 @@ public class SetRadiusFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            radius = getArguments().getFloat(ARG_RADIUS);
         }
-
     }
 
     @Override
@@ -72,13 +61,18 @@ public class SetRadiusFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_set_radius, container, false);
 
-        radiusDisplay = (TextView)view.findViewById(R.id.textRadiusLabel);
+        radiusDisplay = (TextView)view.findViewById(R.id.textRadiusDisplay);
+        radiusDisplay.setText(String.valueOf(radius) + " miles");
+
         radiusValue = (SeekBar)view.findViewById(R.id.seekBarRadiusValue);
         radiusValue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                float radiusInMiles = (progress / 40.0f);
-                radiusDisplay.setText(String.valueOf(radiusInMiles));
+                radius = (progress / 4.0f) + 0.25f;
+                radiusDisplay.setText(String.valueOf(radius) + " miles");
+                if(mListener != null) {
+                    mListener.onFragmentInteraction(radius);
+                }
             }
 
             @Override
@@ -94,21 +88,14 @@ public class SetRadiusFragment extends Fragment {
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof OnSetRadiusFragmentInteractionListener) {
+            mListener = (OnSetRadiusFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement OnSetRadiusFragmentInteractionListener");
         }
     }
 
@@ -128,8 +115,7 @@ public class SetRadiusFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    public interface OnSetRadiusFragmentInteractionListener {
+        void onFragmentInteraction(float radius);
     }
 }
