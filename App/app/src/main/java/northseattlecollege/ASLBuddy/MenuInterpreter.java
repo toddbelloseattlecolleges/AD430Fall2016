a722805d31bd586ec64224f629466849896698c1
Nathan Flint <nlflint@gmail.com>
2016-11-07 22:17:18 -0800

    Interpreter main: Replaced back button with system back button

--- App/app/src/main/java/northseattlecollege/ASLBuddy/MenuInterpreter.java
+++ App/app/src/main/java/northseattlecollege/ASLBuddy/MenuInterpreter.java
@@ -1,10 +1,8 @@
 package northseattlecollege.ASLBuddy;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Author: Nathan Flint
@@ -17,15 +15,12 @@ public class MenuInterpreter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_interpreter);
+    }
 
-        Button backButton = (Button) findViewById(R.id.back);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-                Intent navigationIntent = new Intent(MenuInterpreter.this, LoginActivity.class);
-                MenuInterpreter.this.startActivity(navigationIntent);
-            }
-        });
+    @Override
+    public void onBackPressed() {
+        finish();
+        Intent navigationIntent = new Intent(MenuInterpreter.this, LoginActivity.class);
+        MenuInterpreter.this.startActivity(navigationIntent);
     }
 }
