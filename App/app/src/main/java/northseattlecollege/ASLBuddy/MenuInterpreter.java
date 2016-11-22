5bad5e0a7418c9565bedd380628db53ae27f2b96
Nathan Flint <nlflint@gmail.com>
2016-11-21 17:58:57 -0800

    Implemented async call to get Skype install status

--- App/app/src/main/java/northseattlecollege/ASLBuddy/MenuInterpreter.java
+++ App/app/src/main/java/northseattlecollege/ASLBuddy/MenuInterpreter.java
@@ -1,8 +1,11 @@
 package northseattlecollege.ASLBuddy;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Author: Nathan Flint
@@ -15,7 +18,10 @@ public class MenuInterpreter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_interpreter);
-    }
+
+        UpdateSkypeStatus updateSkypeStatus = new UpdateSkypeStatus();
+        updateSkypeStatus.execute();
+   }
 
     @Override
     public void onBackPressed() {
@@ -23,4 +29,23 @@ public class MenuInterpreter extends AppCompatActivity {
         Intent navigationIntent = new Intent(MenuInterpreter.this, LoginActivity.class);
         MenuInterpreter.this.startActivity(navigationIntent);
     }
+
+    private class UpdateSkypeStatus extends AsyncTask<Void, Void, Boolean> {
+        protected Boolean doInBackground(Void... asdf) {
+
+            return SkypeResources.isSkypeClientInstalled(MenuInterpreter.this);
+        }
+
+        protected void onPostExecute(Boolean isSkypeInstalled) {
+
+            String statusText = isSkypeInstalled ? "Installed" : "Not Installed";
+            int statusTextColor = isSkypeInstalled ? Color.GREEN : Color.RED;
+
+            TextView skypeStatus = (TextView) findViewById(R.id.skypeStatus);
+            skypeStatus.setTextColor(statusTextColor);
+            skypeStatus.setText(statusText);
+        }
+    }
 }
+
+
